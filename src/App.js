a485de8fba8b78dd6c789672c1e5b7ebce857c23
Gerardo García Lopez <gerardoagl96@gmail.com>
2018-11-24 22:02:21 -0400

    Static clients page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom'; 
-import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus, faUsers, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
+
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/layout/Dashboard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+library.add(faPlus, faUsers, faArrowCircleRight);
 
 class App extends Component {
   render() {
@@ -12,7 +17,9 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <div className="container">
-            <h1>Hello World</h1>
+            <Switch>
+              <Route exact path="/" component={Dashboard}/>
+            </Switch>
           </div>
         </div>
       </Router>
