f2ae78ab96e43c8407bf0648607243c2409bd55a
Gerardo García Lopez <gerardoagl96@gmail.com>
2018-11-27 21:25:28 -0400

    Route protection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import { Provider } from 'react-redux';
+
 import store from './redux';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helper/auth';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlus, faUsers, faArrowCircleRight, faArrowCircleLeft, faPencilAlt, faLock } from '@fortawesome/free-solid-svg-icons';
 
@@ -25,11 +27,31 @@ class App extends Component {
             <Navbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard}/>
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/:id" component={Details} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
-                <Route exact path="/login" component={Login} />
+                <Route 
+                  exact 
+                  path="/" 
+                  component={UserIsAuthenticated(Dashboard)}
+                />
+                <Route 
+                  exact 
+                  path="/client/add" 
+                  component={UserIsAuthenticated(AddClient)} 
+                />
+                <Route 
+                  exact 
+                  path="/client/:id" 
+                  component={UserIsAuthenticated(Details)} 
+                />
+                <Route 
+                  exact 
+                  path="/client/edit/:id" 
+                  component={UserIsAuthenticated(EditClient)} 
+                />
+                <Route 
+                  exact 
+                  path="/login" 
+                  component={UserIsNotAuthenticated(Login)} 
+                />
               </Switch>
             </div>
           </div>
