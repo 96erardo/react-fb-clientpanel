08288af472d44e5b1c5e51373051fbaf54c7cbb9
Gerardo García Lopez <gerardoagl96@gmail.com>
2018-11-27 22:34:50 -0400

    Settings reducer, actions and types

---
+++ src/redux/actions/settingsActions.js
@@ -0,0 +1,17 @@
+import {
+    DISABLE_BALANCE_ON_ADD,
+    DISABLE_BALANCE_ON_EDIT,
+    ALLOW_REGISTRATION
+} from './types';
+
+export const setDisableBalanceOnAdd = () => ({
+    type: DISABLE_BALANCE_ON_ADD
+});
+
+export const setDisableBalanceOnEdit = () => ({
+    type: DISABLE_BALANCE_ON_EDIT
+});
+
+export const setAllowRegistration = () => ({
+    type: ALLOW_REGISTRATION
+});
