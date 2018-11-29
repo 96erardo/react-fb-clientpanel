bd024d35cdf4bf4e6670cfbec7342596d300d6a7
Gerardo García Lopez <gerardoagl96@gmail.com>
2018-11-28 20:38:40 -0400

    Settings component

--- src/redux/index.js
+++ src/redux/index.js
@@ -5,6 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
 import { reduxFirestore, firestoreReducer } from 'redux-firestore'
 
 import notifyReducer from './reducers/notifyReducer';
+import settingsReducer from './reducers/settingsReducer';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAPvR0TwaaWg_YIxE-tUsTuBBwxkKSSXnw",
@@ -36,7 +37,8 @@ const createStoreWithFirebase = compose(
 const rootReducer = combineReducers({
     firebase: firebaseReducer,
     firestore: firestoreReducer,
-    notify: notifyReducer
+    notify: notifyReducer,
+    settings: settingsReducer
 });
 
 const initialState = {};
