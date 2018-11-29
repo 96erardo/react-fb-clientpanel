401a5d9a1e7e93f823ce601c54275157edc23ad3
Gerardo García Lopez <gerardo@hostienda.com>
2018-11-29 13:46:29 -0400

    Updates

---
+++ precache-manifest.225a527d1f6473082fde4ea76327c8b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64e6d784bf0f93b4bd91",
+    "url": "/react-fb-clientpanel/static/js/runtime~main.64e6d784.js"
+  },
+  {
+    "revision": "30b16866abd3288eaa14",
+    "url": "/react-fb-clientpanel/static/js/main.30b16866.chunk.js"
+  },
+  {
+    "revision": "ac1a454069d405d5f654",
+    "url": "/react-fb-clientpanel/static/js/1.ac1a4540.chunk.js"
+  },
+  {
+    "revision": "ac1a454069d405d5f654",
+    "url": "/react-fb-clientpanel/static/css/1.1c5d7d2c.chunk.css"
+  },
+  {
+    "revision": "97b0f999a30659df79ade8fc13ad664f",
+    "url": "/react-fb-clientpanel/index.html"
+  }
+];
